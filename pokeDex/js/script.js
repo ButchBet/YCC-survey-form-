31157b53de51e74b521c4463572a273675a1fc13
ButchBet <k.salazar@utp.edu.co>
2021-07-24 00:58:02 -0500

    continue with the pokeDex building, the main files incliding the pokemos.json has been created

---
+++ pokeDex/js/script.js
@@ -0,0 +1,3 @@
+// Arrya with the pokemos direction
+const pokAddress = ["../pokemos/bulbasaur.json", "../pokemos/ivysaur.json", "../pokemos/venusaur.json", "../pokemos/charmander.json",
+                    "../pokemos/charmeleon.json", "../pokemos/charizard.json", "../pokemos/squirtel.json", "../pokemos/wartortle.json", "../pokemos/blastoise.json"];
