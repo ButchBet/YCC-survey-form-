5ff7d0eccd487253a747f96c0212096900b956a9
ButchBet <k.salazar@utp.edu.co>
2021-07-26 22:24:48 -0500

    jamesBondTributePage almost finished, we only need to make it responsive

--- jamesBondTributePage/js/script.js
+++ jamesBondTributePage/js/script.js
@@ -1,4 +1,12 @@
-// configure the normal heigth row
+// Arrays to use in the checkEvent function 
+const alts = ["Daniel Craig as James Bond", "Pierce Brosnan as James Bond",
+              "Timothy Dalton ad James Bond", "Rooger Moore as James Bond",
+              "George Lazenby as James Bond", "Sean Conery as James Bond"];
+
+const actors = ["DanielCraig", "PierceBranson", "TimothyDalton", 
+                "RogerMoore", "GeorgeLazenby", "SeanConnery"];
+
+// Setting of the normal heigth row
 const subcontiner = document.querySelectorAll(".subcontainer");
 subcontiner.forEach(element => {
     // if(element.classList.contains("sub1") || element.classList.contains("sub8")) {
@@ -19,7 +27,7 @@ subcontiner.forEach(element => {
 
 
 
-// change the general back ground to all of the rectangles 
+// Change the general background to all of the rectangles 
 const subSubContainer = document.querySelectorAll(".subSubContainer");
 let firstColor = "colorGray";
 let secondColor = "colorWhite";
@@ -38,5 +46,98 @@ while(i < subSubContainer.length) {
     secondColor = changecolor;
 
     i += 2;
+}
+
+
+// Event click in the different images 
+window.addEventListener("click", (event) => {
+    checkEvent(event);
+});
+
+// function to check the posible events
+function checkEvent(event) {
+    const list = document.querySelector(".list");
+    const container = document.querySelector(".container");
+    
+    // Check if the event is in the close button
+    if(event.path[0].classList.contains("panelButton")) {
+        document.querySelector(".panel").classList.add("hidden");
+        
+        document.querySelectorAll(".subcontainer").forEach(element => {
+            element.classList.remove("opac");
+        });
+
+    // Check is in any of the pictures 
+    } else {
+        for(let i = 0; i < alts.length; i++) {
+            if(event.path[0].alt === alts[i]) {
+                // Remove the last li added
+                list.innerHTML = "";
+
+                const option = {
+                    method : "GET",
+
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }
+
+                const names = fetch("../data/moviesData.json", option);
+                
+                const dates = fetch("../data/dateData.json", option);
+
+                names
+                .then(response => {
+                    if(response.ok) {
+                        return response.json();
+                    } else {
+                        throw new Error(response.status);
+                    }
+                })
+                .then(value => {
+                    dates
+                    .then(response2 => {
+                        if(response2.ok) {
+                            return response2.json();
+                        } else {
+                            throw new Error(response2.status);
+                        }
+                    })
+                    .then(value2 => {
+                        // Loop to go throught the actors movies names
+                        let actor = actors[i];
+                        for(let j = 0; j < value[actor].length; j++) {
+                            let liName = document.createElement("li");
+
+                            let liDate = document.createElement("li");  
+
+                            liName.innerHTML = value[actor][j].toUpperCase();
+
+                            liDate.innerHTML = value2[actor][j];
+                            
+                            liDate.classList.add("lastMod")
+
+                            list.append(liName);
+
+                            list.append(liDate);
+                        }
+                    })
+                    .catch(error2 => {
+                        console.error(error2);
+                    })
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+
+            document.querySelector(".panel").classList.remove("hidden");
+            
+            document.querySelectorAll(".subcontainer").forEach(element => {
+                element.classList.add("opac");
+            });
+            }
+        }
+    }
+    
 }
 
