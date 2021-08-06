4ded0b774c6bd122b330dfe3cb4b2e718e5c187c
ButchBet <k.salazar@utp.edu.co>
2021-08-06 16:06:38 -0500

    Project almost finished, only need to make the js code documentation

--- theMetropolitanMuseumOfArt/js/script.js
+++ theMetropolitanMuseumOfArt/js/script.js
@@ -1,16 +1,41 @@
-var paint = document.getElementsByTagName("img");
+const imgIDs = 
+[   "img1",
+    "img2",
+    "img3",
+    "img4",
+    "img5",
+    "img6",
+    "img7",
+    "img8",
+    "img9",
+    "img10"
+],
+
+    dataIDs = 
+[],
+
+constainer1 = document.getElementById("container1"),
+
+constainer2 = document.getElementById("container2");
 
 document.addEventListener("DOMContentLoaded", () => {
-    let radomID = Math.floor(Math.random() * 45734 + 1);
+    imgIDs.forEach(element => putImages(element, 1))
+});
 
-    let link = "//collectionapi.metmuseum.org/public/collection/v1/objects/" + radomID;
+
+function putImages(img, status) {
+    let link;
+
+    if(status === 1) {
+        let radomID = Math.floor(Math.random() * 500 + 1);
+
+        link = "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + radomID;
+    } else {
+        link = "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + img;
+    }
 
     const options = {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        'Access-Control-Allow-Origin' : 'https://collectionapi.metmuseum.org/'
+        method: "GET"
         };
 
     fetch(link, options)
@@ -19,15 +44,95 @@ document.addEventListener("DOMContentLoaded", () => {
             if(response.ok) {
                 return response.json();
             } else {
-                throw new Error(response.status);
+                putImages(img, 1);
+                return
             }
         })
 
         .then((response) => {
-            console.log(response.constituentWikidata_URL);
-        })
+            if(response.primaryImageSmall === "" 
+               || response.country === ""
+               || response.artistDisplayName === ""
+               || response.title === "") {
+                putImages(img), 1;
+                return;
+            } else {
+                if(status === 1) {
+                    dataIDs.push(response.objectID);
 
-        .catch((error) => {
-            console.log(error);
+                    console.log(response);
+
+                    const division = document.getElementById(img);
+
+                    const image = document.createElement("img");
+                    
+                    image.src = response.primaryImageSmall;
+
+                    image.alt = response.objectID;
+
+                    division.append(image);
+                } else if(status === 0) {
+                    console.clear();
+
+                    console.log(response.artistDisplayName);
+                    console.log(response.country)
+                    // Presentation ariables 
+                    const title = document.getElementById("title"),
+
+                    artist = document.getElementById("artist"),
+                    
+                    country = document.getElementById("country"),
+
+                    dimensions = document.getElementById("dimensions"),
+
+                    medium = document.getElementById("medium"),
+
+                    date = document.getElementById("date"), 
+
+                    seeMore = document.getElementById("button2");
+
+                    // change the values
+
+                     title.innerHTML = response.title;
+
+                     artist.innerHTML = response.artistDisplayName;
+
+                     country.innerHTML = response.country;
+
+                     dimensions.innerHTML = response.dimensions;
+
+                     medium.innerHTML = response.medium;
+
+                     date.innerHTML = response.objectDate;
+
+                    seeMore.href = response.objectURL;
+
+                     constainer2.style.display = "flex";
+
+                     constainer1.classList.add("hidden");
+
+                }
+            }
+            // document.getElementById(img).src = response.primaryImageSmall;
         });
+}
+
+ // Event catch
+ document.addEventListener("click", event => {
+    eventCheck(event);
 });
+
+// function to check the event 
+function eventCheck(event) {
+    if(event.path[0].id === "button1") {
+        constainer2.style.display = "none";
+
+        constainer1.classList.remove("hidden");
+    }
+
+    dataIDs.forEach(element => {
+        if(element == event.path[0].alt) {
+            putImages(event.path[0].alt, 0);
+        }
+    });
+}
